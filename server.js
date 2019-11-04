45f82e176f8d703ddc52e0294b38b1ea83b47018
saeful.millah0607 <saeful.millah0607@gmail.com>
2019-11-04 16:39:47 +0700

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser');
-	port = process.env.PORT || 6666;
+	port = process.env.PORT || 8666;
 // cors
 const cors = require('cors');
 app.use(cors());
